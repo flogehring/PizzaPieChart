aaa5d0bcdb801757a0dee628057541d8d7b39092
Flo Gehring <flo.gehring@googlemail.com>
2021-09-23 23:00:09 +0200

    added resizeing

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,35 @@ import { makeDayChart } from "./dayChart";
 import { makeMonthChart } from "./monthChart";
 import { makeWeekdayChart } from "./weekdayChart";
 import { dates } from "./pieDays";
+import * as echarts from "echarts/core";
 
-const years = [2021, 2020];
+function makeDayCharts() {
+  const years = [2021, 2020];
 
-years.forEach((year) => {
-  const el = document.getElementById("dayCharts");
-  const mountEl = document.createElement("div");
-  mountEl.classList.add("chart");
-  mountEl.classList.add("dayChart");
-  el?.appendChild(mountEl);
-  makeDayChart(dates, year, mountEl);
-});
+  years.forEach((year) => {
+    const el = document.getElementById("dayCharts");
+    const mountEl = document.createElement("div");
+    mountEl.classList.add("chart");
+    mountEl.classList.add("dayChart");
+    el?.appendChild(mountEl);
+    makeDayChart(dates, year, mountEl);
+  });
+}
 
-//makeDayChart(dates, "2021", "dayChart");
+function resizeCharts() {
+  const wrappers = document.querySelectorAll(".chart");
+  wrappers.forEach((wrapper) => {
+    const instance = echarts.getInstanceByDom(wrapper as HTMLElement);
+    instance.resize();
+  });
+}
+
+makeDayCharts();
 
 makeMonthChart(dates);
 
 makeWeekdayChart(dates);
+
+window.addEventListener("resize", () => {
+  resizeCharts();
+});
